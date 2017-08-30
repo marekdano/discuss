1d7e6433a7ecf2bd6a34b264a26e561809ea61e2
Marek Dano <mk.dano@gmail.com>
2017-08-30 08:56:00 +0100

    Add view and template for 'topic'.

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,7 +1,14 @@
 defmodule Discuss.TopicController do
 	use Discuss.Web, :controller
 
+	alias Discuss.Topic
+
 	def new(conn, params) do
-		
+		# struct = %Topic{}
+		# params = %{}
+		# changeset = Topic.changeset(struct, params)
+		changeset = Topic.changeset(%Topic{}, %{})
+
+		render conn, "new.html"
 	end
 end
