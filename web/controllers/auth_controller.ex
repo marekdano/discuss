062e268a9e3bde4ad338db90ad4a26bc091848f1
Marek Dano <mk.dano@gmail.com>
2017-09-12 09:07:56 +0100

    Add 'user' model with the changset.

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -2,11 +2,11 @@ defmodule Discuss.AuthController do
 	use Discuss.Web, :controller
 	plug Ueberauth
 
-	def callback(conn, params) do
-		IO.puts "++++++++++++++"
-		IO.inspect(conn.assigns)
-		IO.puts "++++++++++++++"
-		IO.inspect(params)
-		IO.puts "++++++++++++++"
+	alias Discuss.User
+
+	def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+		user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+		changeset = User.changeset(%User{}, user_params)
+		
 	end
 end
