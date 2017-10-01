ea2e98ffb9505bb91bfd4cb316b6d917a7abdd5d
Marek Dano <mk.dano@gmail.com>
2017-10-01 12:59:03 +0100

    Fetch Topics on channel join method.

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,10 +1,14 @@
 defmodule Discuss.CommentsChannel do
 	use Discuss.Web, :channel
+	alias Discuss.Topic
 
-	def join(name, _params, socket) do
-		IO.puts("+++++++")
-		IO.puts(name)
-		{:ok, %{hey: "there"}, socket}
+	def join("comments:" <> topic_id, _params, socket) do
+		# IO.puts("+++++++")
+		# IO.puts(topic_id)
+		topic_id = String.to_integer(topic_id)
+		topic = Repo.get(Topic, topic_id)
+		
+		{:ok, %{}, socket}
 	end
 
 	def handle_in(name, message, socket) do
