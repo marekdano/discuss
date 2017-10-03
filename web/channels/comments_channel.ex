daa8a420b03b3f6930a913af5cdd17a186fcf669
Marek Dano <mk.dano@gmail.com>
2017-10-03 14:27:54 +0100

    Remove server logs.

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -3,8 +3,6 @@ defmodule Discuss.CommentsChannel do
 	alias Discuss.Topic
 
 	def join("comments:" <> topic_id, _params, socket) do
-		# IO.puts("+++++++")
-		# IO.puts(topic_id)
 		topic_id = String.to_integer(topic_id)
 		topic = Repo.get(Topic, topic_id)
 		
@@ -12,9 +10,6 @@ defmodule Discuss.CommentsChannel do
 	end
 
 	def handle_in(name, %{"content" => content}, socket) do
-		IO.puts("+++++++")
-		IO.puts(name)
-		IO.inspect(content)
 
 		{:reply, :ok, socket}
 	end
